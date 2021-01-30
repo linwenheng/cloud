d216745e67ea8d9448ac9ecc961967d42f038c0b
Lin <1972073918@qq.com>
2021-01-30 10:00:10 +0800

    2

---
+++ src/main/java/com/project/XXcloud/SparkSense/SparkSense.scala
@@ -0,0 +1,66 @@
+package com.project.XXcloud.SparkSense
+
+import java.io.File
+
+import XMLUtil.XMLUtil
+import com.project.XXcloud.HDFS.HDFSOperation
+import com.project.XXcloud.SplitWords.SplitWords
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object SparkSense {
+  private val hdfsConf = XMLUtil.getHdfsConf();
+  private val ip = hdfsConf(0);
+  private val port = hdfsConf(1);
+  private var conf: SparkConf = null;
+  private var sc: SparkContext = null;
+
+
+  /**
+   *
+   * */
+  private def initialSpark(appName: String): Unit = {
+    conf = new SparkConf().setAppName(appName).setMaster("local[*]");
+    sc = new SparkContext(conf);
+
+  }
+
+
+
+
+  /**
+   * 功能：分析单个文本文件，屏蔽敏感词
+   * 传入参数：参数1：用户文件夹名称（即用户邮箱），参数2：文件名
+   * 返回值：无
+   * */
+  def analyzeTextFile(email: String, fileName: String): Unit = {
+    initialSpark(email + "_" + fileName);
+
+    val data = sc.textFile("hdfs://" + ip + ":" + port + "/" + email + "/" + fileName);
+    data.map(x =>SplitWords.splitWords(x)).repartition(1).saveAsTextFile("C:\\Users\\Amaze\\Desktop\\testFiles\\" + email + "tmp");
+
+    HDFSOperation.writeFile(email, fileName, "C:\\Users\\Amaze\\Desktop\\testFiles\\" + email + "tmp\\part-00000");
+    val path: File = new File("C:\\Users\\Amaze\\Desktop\\testFiles\\" + email + "tmp");
+    deleteDir(path);
+
+  }
+
+
+  /**
+   * 删除一个文件夹,及其子目录
+   *
+   * @param dir
+   */
+  private def deleteDir(dir: File): Unit = {
+    val files = dir.listFiles()
+    files.foreach(f => {
+      if (f.isDirectory) {
+        deleteDir(f)
+      } else {
+        f.delete()
+      }
+    })
+    dir.delete()
+
+  }
+}
---
+++ src/main/java/com/project/XXcloud/SplitWords/SplitWords.java
@@ -0,0 +1,79 @@
+package com.project.XXcloud.SplitWords;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class SplitWords
+{
+    private static String[] sensivesWords=SensiveWords.sensiveWords();
+    public static String splitWords(String line)
+    {
+        //保存返回结果
+        StringBuilder sb=new StringBuilder(line);
+
+        //构建IK分词器，使用smart分词模式
+        Analyzer analyzer = new IKAnalyzer(true);
+
+        //获取Lucene的TokenStream对象
+        TokenStream ts = null;
+        try {
+            ts = analyzer.tokenStream("myfield", new StringReader(line));
+            //获取词元位置属性
+            OffsetAttribute  offset = ts.addAttribute(OffsetAttribute.class);
+            //获取词元文本属性
+            CharTermAttribute term = ts.addAttribute(CharTermAttribute.class);
+            //获取词元文本属性
+            TypeAttribute type = ts.addAttribute(TypeAttribute.class);
+
+
+            //重置TokenStream（重置StringReader）
+            ts.reset();
+            //迭代获取分词结果
+            while (ts.incrementToken()) {
+                //System.out.println(offset.startOffset() + " - " + offset.endOffset() + " : " + term.toString() + " | " + type.type());
+                if(checkSensive(term.toString()))
+                {
+                    int num=offset.endOffset()-offset.startOffset();
+                    String s_replace="";
+                    for(int i=0;i<num;++i)
+                    {
+                        s_replace+="*";
+                    }
+                    sb.replace(offset.startOffset(),offset.endOffset(),s_replace);
+                }
+            }
+            //关闭TokenStream（关闭StringReader）
+            ts.end();   // Perform end-of-stream operations, e.g. set the final offset.
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //释放TokenStream的所有资源
+            if(ts != null){
+                try {
+                    ts.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private static boolean checkSensive(String word)
+    {
+        for(String s:sensivesWords)
+        {
+            if(s.equals(word))
+                return true;
+        }
+        return false;
+    }
+}
+
